af245d5f7744eb35d3d2c612e33d289890e1d0f5
BoRn_SliPPy <143754651+BRN-SLP@users.noreply.github.com>
2026-07-05 06:10:28 +0300

    update inline documentation (#193)
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>

--- src/guards_add_validator_check.ts
+++ src/guards_add_validator_check.ts
@@ -3,6 +3,9 @@ export type guards_add_validator_checkResult<T> = {
   error: string | null;
 };
 
+/** wrapResult - performs core operation */
+/** @returns result of the operation */
+/** @param params - input parameters */
 export function wrapResult<T>(data: T): guards_add_validator_checkResult<T> {
   return { data, error: null };
 }
