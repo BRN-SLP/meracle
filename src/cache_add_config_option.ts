0852279241a3f56eb5bca9ed448a93fb092ed143
Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>
2026-07-05 07:39:50 +0300

    add helper utility for common operations

---
+++ src/cache_add_config_option.ts
@@ -0,0 +1,8 @@
+export type cache_add_config_optionResult<T> = {
+  data: T | null;
+  error: string | null;
+};
+
+export function wrapResult<T>(data: T): cache_add_config_optionResult<T> {
+  return { data, error: null };
+}
