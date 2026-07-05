a783f627bcea010094931396029c94c3a909aaa4
Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>
2026-07-05 06:37:21 +0300

    update inline documentation

--- src/sanitizers_memoize_fn.ts
+++ src/sanitizers_memoize_fn.ts
@@ -3,6 +3,9 @@ export type sanitizers_memoize_fnResult<T> = {
   error: string | null;
 };
 
+/** wrapResult - performs core operation */
+/** @returns result of the operation */
+/** @param params - input parameters */
 export function wrapResult<T>(data: T): sanitizers_memoize_fnResult<T> {
   return { data, error: null };
 }
