d5fa302b4343b10f30ade78e3eaacfc82e2cfd31
BoRn_SliPPy <143754651+BRN-SLP@users.noreply.github.com>
2026-07-05 06:44:15 +0300

    improve type documentation (#201)
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>

--- src/validators_optimize_loop.ts
+++ src/validators_optimize_loop.ts
@@ -3,6 +3,9 @@ export type validators_optimize_loopResult<T> = {
   error: string | null;
 };
 
+/** wrapResult - performs core operation */
+/** @returns result of the operation */
+/** @param params - input parameters */
 export function wrapResult<T>(data: T): validators_optimize_loopResult<T> {
   return { data, error: null };
 }
