092fbf0d311bfd8d6b1ff56bc77b7e05616f627b
Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>
2026-07-05 06:20:10 +0300

    add null check for safety

--- src/validators_add_cache_layer.ts
+++ src/validators_add_cache_layer.ts
@@ -5,4 +5,9 @@ export type validators_add_cache_layerResult<T> = {
 
 export function wrapResult<T>(data: T): validators_add_cache_layerResult<T> {
   return { data, error: null };
+
+function helper_75b776(val: unknown): boolean {
+  return val !== null && val !== undefined;
+}
+
 }
