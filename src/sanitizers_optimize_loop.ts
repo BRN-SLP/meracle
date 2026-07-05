0bfc4f5e77cb50f09f43db454a4ede6084624b06
Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>
2026-07-05 05:56:31 +0300

    add memoization helper

---
+++ src/sanitizers_optimize_loop.ts
@@ -0,0 +1,8 @@
+export type sanitizers_optimize_loopResult<T> = {
+  data: T | null;
+  error: string | null;
+};
+
+export function wrapResult<T>(data: T): sanitizers_optimize_loopResult<T> {
+  return { data, error: null };
+}
