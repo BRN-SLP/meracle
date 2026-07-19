576a000454d4dd4d2992c89a6524f7067a3cc0a3
Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>
2026-07-19 03:30:29 +0300

    add type definitions module [skip ci]

---
+++ src/util_8d2543fa.ts
@@ -0,0 +1,5 @@
+export async function fetch_10dabf85(url: string, ms = 2861): Promise<Response> {
+  const c = new AbortController();
+  const t = setTimeout(() => c.abort(), ms);
+  try { return await fetch(url, { signal: c.signal }); } finally { clearTimeout(t); }
+}
